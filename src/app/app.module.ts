3150104a6ccab6c9ea70949f10dfeae32e6ff2a8
heja99 <janhery99@gmail.com>
2021-10-28 18:43:04 +0200

    Programmplanung - Tabelle und Eingabefeld angelegt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProgrammplanungComponent } from './programmplanung/programmplanung.comp
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatTableModule} from '@angular/material/table';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
@@ -43,6 +44,7 @@ import { exportReducer } from './store/export/export.reducer';
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
+    MatTableModule,
     FormsModule,
     MatCardModule,
     MatInputModule,
--- src/app/programmplanung/programmplanung.component.ts
+++ src/app/programmplanung/programmplanung.component.ts
@@ -1,13 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Endprodukte {
+  artikelnummer: number;
+  aktueller_lagerbestand: number;
+  in_bearbeitung: number;
+  in_warteschlange: number;
+  geplanter_endbestand: number;
+  vertriebswunsch: number;
+  direktverkauf: number;
+  produktionsauftraege: number;
+}
+
+// Testdaten
+const ENDPRODUKT_DATEN: Endprodukte[] = [
+  {artikelnummer: 1, aktueller_lagerbestand: 300, in_bearbeitung: 100, in_warteschlange: 0, geplanter_endbestand: 100, vertriebswunsch: 50, direktverkauf: 200, produktionsauftraege: 300},
+  {artikelnummer: 2, aktueller_lagerbestand: 200, in_bearbeitung: 50, in_warteschlange: 0, geplanter_endbestand: 50, vertriebswunsch: 50, direktverkauf: 10, produktionsauftraege: 300},
+  {artikelnummer: 3, aktueller_lagerbestand: 150, in_bearbeitung: 100, in_warteschlange: 50, geplanter_endbestand: 60, vertriebswunsch: 50, direktverkauf: 80, produktionsauftraege: 250}
+];
+
 @Component({
   selector: 'app-programmplanung',
   templateUrl: './programmplanung.component.html',
   styleUrls: ['./programmplanung.component.scss']
 })
+
+
 export class ProgrammplanungComponent implements OnInit {
 
-  constructor() { }
+  // Tabelle
+  displayedColumns: string[] = ['artikelnummer', 'aktueller_lagerbestand', 'in_bearbeitung', 'in_warteschlange', 'geplanter_endbestand', 'vertriebswunsch', 'direktverkauf', 'produktionsauftraege'];
+  dataSource = ENDPRODUKT_DATEN;
+  // Eingabefeld
+  
+
+  constructor() { 
+  }
 
   ngOnInit(): void {
   }
