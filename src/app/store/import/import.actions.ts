acaaa6074237098725b7dabc7af03b3b2502fad2
Leon Wagner <wagner_leon@gmx.de>
2021-11-16 16:14:07 +0100

    programmplanung

--- src/app/store/import/import.actions.ts
+++ src/app/store/import/import.actions.ts
@@ -2,3 +2,4 @@ import { createAction } from '@ngrx/store';
 import {ImportModel} from "../../model/import.model";
 
 export const addImportXml = createAction('[Dateiimport Component] Add XML_IMPORT', (importModel: ImportModel ) => ({importModel}));
+
