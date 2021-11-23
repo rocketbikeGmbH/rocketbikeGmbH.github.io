ad90eeb54821254996e4cb443904aecea7739fd3
S.K <62422252+SergioKl@users.noreply.github.com>
2021-11-23 11:29:00 +0100

    infobutton added

---
+++ src/app/infobutton/infobutton.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Inject} from '@angular/core';
+
+@Component({
+  selector: 'app-infobutton',
+  templateUrl: './infobutton.component.html',
+  styleUrls: ['./infobutton.component.scss']
+})
+export class InfobuttonComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+}
